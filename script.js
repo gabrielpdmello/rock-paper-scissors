804e304ec31724f6bd7f95dce8e5909e693a18df
Gabriel Mello <gabrielpdmello@gmail.com.com>
2025-08-08 21:28:10 -0300

    Improve UI

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const buttonRock = document.querySelector(".rock");
 const buttonPaper = document.querySelector(".paper");
 const buttonScissor = document.querySelector(".scissor");
-
-const results = document.querySelector(".results");
-const roundResult = document.querySelector(".round-result");
-const displayHumanScore = document.querySelector(".human-score");
+const gameContainer = document.querySelector(".game-container");
 const displayRound = document.querySelector(".round");
+const displayHumanScore = document.querySelector(".human-score");
 const displayComputerScore = document.querySelector(".computer-score")
 const displayTies = document.querySelector(".ties");
-const displayResult = document.querySelector(".game-result");
+const gameMessage = document.querySelector(".game-message");
+const gameResult = document.querySelector(".game-result");
 
 let round = 0;
 let computerScore = 0;
 let humanScore = 0;
 let ties = 0;
+let gameIsRunning = true;
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
@@ -27,81 +27,81 @@ function getComputerChoice() {
 }
 
 function playRound(computerChoice, humanChoice) {
-    if (computerChoice === humanChoice) {
-        roundResult.textContent = `Tie! both chose ${computerChoice}`;
-        ties++;
-        displayTies.textContent = `Ties: ${ties}`;
+    if (gameIsRunning) {
+        if (computerChoice === humanChoice) {
+            gameMessage.textContent = `Tie! both chose ${computerChoice}`;
+            ties++;
+            displayTies.textContent = `${ties}`;
 
-    } else if (computerChoice === "rock" && humanChoice === "paper") {
-        roundResult.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
-        humanScore++;
-        displayHumanScore.textContent = `Player: ${humanScore}`;
+        } else if (computerChoice === "rock" && humanChoice === "paper") {
+            gameMessage.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+            humanScore++;
+            displayHumanScore.textContent = `${humanScore}`;
 
-    } else if (computerChoice === "paper" && humanChoice === "scissor") {
-        roundResult.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
-        humanScore++;
-        displayHumanScore.textContent = `Player: ${humanScore}`;
-        
-    } else if (computerChoice === "scissor" && humanChoice === "rock") {
-        roundResult.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
-        humanScore++;
-        displayHumanScore.textContent = `Player: ${humanScore}`;
+        } else if (computerChoice === "paper" && humanChoice === "scissor") {
+            gameMessage.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+            humanScore++;
+            displayHumanScore.textContent = `${humanScore}`;
+            
+        } else if (computerChoice === "scissor" && humanChoice === "rock") {
+            gameMessage.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+            humanScore++;
+            displayHumanScore.textContent = `${humanScore}`;
 
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
-        roundResult.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
-        computerScore++;
-        displayComputerScore.textContent = `Computer: ${computerScore}`;
+        } else if (humanChoice === "rock" && computerChoice === "paper") {
+            gameMessage.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+            computerScore++;
+            displayComputerScore.textContent = `${computerScore}`;
 
-    } else if (humanChoice === "paper" && computerChoice === "scissor") {
-        roundResult.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
-        computerScore++;
-        displayComputerScore.textContent = `Computer: ${computerScore}`;
+        } else if (humanChoice === "paper" && computerChoice === "scissor") {
+            gameMessage.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+            computerScore++;
+            displayComputerScore.textContent = `${computerScore}`;
 
-    } else if (humanChoice === "scissor" && computerChoice === "rock") {
-        roundResult.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
-        computerScore++;
-        displayComputerScore.textContent = `Computer: ${computerScore}`;
-    }
-
-    round++;
-    displayRound.textContent = `Round ${round}`;
-
-    if (round >= 5) {
-        if (computerScore > humanScore) {
-            displayResult.textContent = "Game result: You lost!"
-        } else if (computerScore < humanScore) {
-            displayResult.textContent = "Game result: You won!"
-        } else {
-            displayResult.textContent = "Game result: Tie!"
+        } else if (humanChoice === "scissor" && computerChoice === "rock") {
+            gameMessage.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+            computerScore++;
+            displayComputerScore.textContent = `${computerScore}`;
         }
-    
-        buttonRock.style.display = "none";
-        buttonPaper.style.display = "none";
-        buttonScissor.style.display = "none";
-    
-        const resetBtn = document.createElement("button");
-        resetBtn.textContent = "play again?";
-        results.appendChild(resetBtn);
-        resetBtn.addEventListener("click", ()=> {
-            results.removeChild(resetBtn);
 
-            round = 0;
-            computerScore = 0;
-            humanScore = 0;
-            ties = 0;
-    
-            roundResult.textContent = "";
-            displayHumanScore.textContent = "Player:";
-            displayRound.textContent = "Round";
-            displayComputerScore.textContent = "Computer:";
-            displayTies.textContent = "Ties:";
-            displayResult.textContent = "";
+        round++;
+        displayRound.textContent = `Round ${round}`;
 
-            buttonRock.style.display = "inline-block";
-            buttonPaper.style.display = "inline-block";
-            buttonScissor.style.display = "inline-block";
-        })
+        if (round > 4) {
+            gameIsRunning = false;
+            if (computerScore > humanScore) {
+                gameResult.textContent += "Game result: You lost!";
+            } else if (computerScore < humanScore) {
+                gameResult.textContent += "Game result: You won!";
+            } else {
+                gameResult.textContent += "Game result: Tie!";
+            }
+        
+            const resetBtn = document.createElement("button");
+            resetBtn.textContent = "play again?";
+            resetBtn.classList.add("reset-btn");
+            gameContainer.appendChild(resetBtn);
+            resetBtn.addEventListener("click", ()=> {
+                gameContainer.removeChild(resetBtn);
+
+                round = 0;
+                computerScore = 0;
+                humanScore = 0;
+                ties = 0;
+                gameIsRunning = true
+        
+                displayRound.textContent = "Round";
+                displayHumanScore.textContent = "0";
+                displayTies.textContent = "0";
+                displayComputerScore.textContent = "0";
+
+                gameMessage.textContent = "Click to play!";
+                gameResult.textContent = "";
+
+            })
+        }
     }
+    
 }
 
 buttonRock.addEventListener("click", () => {
